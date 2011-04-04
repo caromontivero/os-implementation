0e68ffd17a8d2e6ccb564437538f26293151b44c
Carolina Montivero <caromontivero@gmail.com>
2011-04-04 11:18:28 -0300

    Refactoring

--- src/project1/src/geekos/elf.c
+++ src/project1/src/geekos/elf.c
@@ -34,9 +34,6 @@
 #define EM_860		7
 #define EM_MIPS		8
 
-#define EV_NONE		0
-#define EV_CURRENT	1
-
 #define EI_MAG0		0
 #define EI_MAG1		1
 #define EI_MAG2		2
@@ -46,17 +43,42 @@
 #define ELFMAG2		'L'
 #define ELFMAG3		'F'
 
-#define PT_NULL		0
-#define PT_LOAD		1
-#define PT_DYNAMIC	2
-#define PT_INTERP	3
-#define PT_NOTE		4
-#define PT_SHLIB	5
-#define PT_PHDR		6
-#define PT_LOCPROC	0x70000000
-#define PT_HIPROC	0x7fffffff
+//Returned value:
+// 0 = success
+// != 0 Failure
 
+int Parse_ELF_Header(char *exeFileData, ulong_t exeFileLength, elfHeader *elf_hdr)
+{
+    int result = -1;
+    if(sizeof(elfHeader) < exeFileLength)
+    {
+        memcpy(elf_hdr,exeFileData,sizeof(elfHeader));
+        result = 0;
+    }
+    return result;
+}
 
+bool Is_Valid_ELF(elfHeader *elf_hdr)
+{
+    return ( (elf_hdr->type == ET_EXEC) && (elf_hdr->machine == EM_386) && (elf_hdr->ident[EI_MAG0] == ELFMAG0) && (elf_hdr->ident[EI_MAG1] == ELFMAG1) && (elf_hdr->ident[EI_MAG2] == ELFMAG2) && (elf_hdr->ident[EI_MAG3] == ELFMAG3) ); 
+}
+
+void Load_Program_Headers(char *exeFileData, struct Exe_Segment *segmentList, int offset, int numProgs)
+{
+    int count = 0;
+    programHeader p_hdr;
+
+    while(count < numProgs)
+    {
+        memcpy(&p_hdr,&exeFileData[offset + sizeof(programHeader) * count],sizeof(programHeader));
+        segmentList[count].offsetInFile = p_hdr.offset;
+        segmentList[count].lengthInFile = p_hdr.fileSize;
+        segmentList[count].startAddress = p_hdr.vaddr;
+        segmentList[count].sizeInMemory = p_hdr.memSize;
+        segmentList[count].protFlags = p_hdr.flags;
+        count++;  
+    }
+}
 /**
  * From the data of an ELF executable, determine how its segments
  * need to be loaded into memory.
@@ -69,35 +91,23 @@
 int Parse_ELF_Executable(char *exeFileData, ulong_t exeFileLength,
     struct Exe_Format *exeFormat)
 {
-      int result = 0;
-      elfHeader e_hdr;
-      int i = 0;
-      programHeader p_hdr;
-      
-      memcpy(&e_hdr,exeFileData,sizeof(elfHeader));
-      if( (e_hdr.type != ET_EXEC) && 
-        (e_hdr.machine != EM_386) && 
-        (e_hdr.ident[EI_MAG0] != ELFMAG0) && (e_hdr.ident[EI_MAG1] != ELFMAG1) && (e_hdr.ident[EI_MAG2] != ELFMAG2) && (e_hdr.ident[EI_MAG3] != ELFMAG3) )
+      int result = -1;
+      elfHeader *e_hdr = NULL;
+      e_hdr = Malloc(sizeof(elfHeader));
+      if(e_hdr != NULL)
       {
-          result = -1;
-      }
-      else
-      {
-          exeFormat->numSegments = e_hdr.phnum;            	
-          //Load program headers
-          while(i < e_hdr.phnum)
+          if(Parse_ELF_Header(exeFileData,exeFileLength,e_hdr) == 0)
           {
-              memcpy(&p_hdr,&exeFileData[e_hdr.phoff + sizeof(programHeader) * i],sizeof(programHeader));
-              exeFormat->segmentList[i].offsetInFile = p_hdr.offset;
-              exeFormat->segmentList[i].lengthInFile = p_hdr.fileSize;
-              exeFormat->segmentList[i].startAddress = p_hdr.vaddr;
-              exeFormat->segmentList[i].sizeInMemory = p_hdr.memSize;
-              exeFormat->segmentList[i].protFlags = p_hdr.flags;
-              i++;
+              if(Is_Valid_ELF(e_hdr))
+              {
+	  exeFormat->numSegments = e_hdr->phnum;
+	  Load_Program_Headers(exeFileData,exeFormat->segmentList,e_hdr->phoff, exeFormat->numSegments);
+	  result = 0;
+              }
+              Free(e_hdr);
+              e_hdr = NULL;
           }
-
       }
-        
       return result;
 }
 
